1a643f3ba231f3c4a83eb42118792b5f41e71c8d
odetayo dorcas <odetayodorcas123@gmail.com>
2024-06-18 15:01:19 +0100

    create portfolio card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='w-11/12  mt-32 mx-5 text-stone-300 py-2.5 px-2.5 rounded-3xl'>
       <div className=' text-center'>
-        <p className=' font-semibold'>© 2024 Dorcas / Designed and Developed by Dorcas</p>
+        <p className=' font-semibold'>© 2024 | Designed & coded ❤ by Dorcas Odetayo</p>
       </div>
     </footer>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,133 @@
-import React from 'react'
+import form from '../assets/website/form.png'
+import portfolio from '../assets/website/portfolio.png'
+import rps from '../assets/website/rps.png'
+import skill from '../assets/website/skill.png'
+import codeicon from '../assets/code-fork.gif'
+import eyeicon from '../assets/eye.gif'
 
 function Project() {
   return (
-    <div>Project</div>
+    <div className='mx-3 my-3'>
+      <div className='flex gap-10 items-center justify-center flex-col'>
+        <div className='my-4 '>
+          <p className='sm:text-4xl my-5 text-2xl font-semibold'>Showcase of my Personal Projects</p>
+          <p className='text-xl my-5'>Browse through a collection of my creations to gain insight into my skills and interests.</p>
+        </div>
+
+        <div >
+          <div className="flex flex-col justify-center items-center gap-10">
+            <div className="w-64 card rounded sm:w-3/6">
+              <div>
+                <img className='rounded' src={skill} alt="project-img" />
+              </div>
+              <div className='mx-3 my-3 px-3 py-3 flex   gap-3 flex-col'>
+                <p className='text-lg font-semibold'>Skillhub </p>
+                <p className='text-gray-300'>Online Learning platform</p>
+                <p>Html css js</p>
+                <div className="flex items-center  gap-10">
+                  <div>
+                    <div className='flex items-center gap-1'>
+                      <img className='w-8' src={codeicon} alt="icon" />
+                      <div>
+                        <a href="https://github.com/dorcasgold/skillhub">code</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex items-center gap-1'>
+                    <img className='w-6' src={eyeicon} alt="icon" />
+                    <div>
+                      <a href="https://skillhubb.netlify.app/">Live Demo</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-64 card rounded sm:w-3/6">
+              <div>
+                <img className='rounded' src={portfolio} alt="project-img" />
+              </div>
+              <div className='mx-3 my-3 px-3 py-3 flex   gap-3 flex-col'>
+                <p className='text-lg font-semibold'>Portfolio Website</p>
+                <p className='text-gray-300'>My previous Portfolio Website</p>
+                <p>Html css js</p>
+                <div className="flex items-center  gap-10">
+                  <div>
+                    <div className='flex items-center gap-1'>
+                      <img className='w-8' src={codeicon} alt="icon" />
+                      <div>
+                        <a href="https://github.com/dorcasgold/dorcasodetayo">code</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex items-center gap-1'>
+                    <img className='w-6' src={eyeicon} alt="icon" />
+                    <div>
+                      <a href="https://dorcas-odetayo.netlify.app/">Live Demo</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-64 card rounded sm:w-3/6">
+              <div>
+                <img className='rounded' src={form} alt="project-img" />
+              </div>
+              <div className='mx-3 my-3 px-3 py-3 flex   gap-3 flex-col'>
+                <p className='text-lg font-semibold'>Login / Signup </p>
+                <p className='text-gray-300'>a modern and user-friendly login/signup form</p>
+                <p>React Tailwind </p>
+                <div className="flex items-center  gap-10">
+                  <div>
+                    <div className='flex items-center gap-1'>
+                      <img className='w-8' src={codeicon} alt="icon" />
+                      <div>
+                        <a href="https://github.com/dorcasgold/React-Login-Signup">code</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex items-center gap-1'>
+                    <img className='w-6' src={eyeicon} alt="icon" />
+                    <div>
+                      <a href="https://react-form-dorcas.netlify.app/">Live Demo</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-64 card rounded sm:w-3/6">
+              <div>
+                <img className='rounded' src={skill} alt="project-img" />
+              </div>
+              <div className='mx-3 my-3 px-3 py-3 flex   gap-3 flex-col'>
+                <p className='text-lg font-semibold'>Skillhub </p>
+                <p className='text-gray-300'>Online Learning platform</p>
+                <p>Html css js</p>
+                <div className="flex items-center  gap-10">
+                  <div>
+                    <div className='flex items-center gap-1'>
+                      <img className='w-8' src={codeicon} alt="icon" />
+                      <div>
+                        <a href="#">code</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex items-center gap-1'>
+                    <img className='w-6' src={eyeicon} alt="icon" />
+                    <div>
+                      <a href="#">Live Demo</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
